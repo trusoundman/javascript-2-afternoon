2b244ba30b45bbaa140d3d042e4ef704bf4ef582
Truman Proctor <trumanproctor@Trumans-MacBook-Pro.local>
2018-11-11 23:51:07 -0700

    1.2 Object Fiddles Assignment

--- practice.js
+++ practice.js
@@ -14,8 +14,13 @@
   Then alert your name using dot notation.
 */
 
-//Code here
-
+var me = {
+  name: "Truman",
+  age: 31
+};
+function alert(){
+  alert(me.name);
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -25,24 +30,30 @@
   Have the values to those keys be your favorite thing in that category.
 */
 
-//Code here
-
+var favoriteThings = {
+  band: "A Great Big World",
+  food: "Italian",
+  person: "My wife & son",
+  book: "Jurassic Park",
+  movie: "Interstellar",
+  holiday: "Christmas"
+};
 
 
 /*
   After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
 */
 
-//Code here
-
+favoriteThings.car = "Honda";
+favoriteThings.brand = "Canon";
 
 
 /*
   Now change the value of the food key in your favoriteThings object to be 'Chicken Nuggets' and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
 */
 
-//Code here
-
+favoriteThings.food = "Chicken Nuggets";
+favoriteThings.book = "Harry Potter";
 
 
 ////////// PROBLEM 3 //////////
@@ -55,15 +66,20 @@
   Using dot notation, add another key (or property) to your backPack object that is named color, with the value being the color of your backpack. 
 */
 
-//Code here
-
-
+var backPack = {};
+var item = "firstPocket";
+backPack.firstPocket = item;
+backPack.firstPocket = 'chapstick';
+backPack.color = "Blue";
 
 /*
   After you do the above, alert your entire backPack object.
 */
 
-//Code here
+function myAlert(object){
+  alert(backPack);
+};
+myAlert(backPack);
 
 
 
@@ -73,8 +89,7 @@ Alerting to see the data in your Object doesn't work so well.
 Instead, console.log your whole backPack object and then check out the console. 
 */
 
-//Code here
-
+console.log(backPack);
 
 
 ////////// PROBLEM 4 //////////
@@ -96,7 +111,8 @@ var user2 = {
   Make that change without modifying the original object code above.
 */
 
-//Code Here
+user2.name = 'Tyler S. McGinnis';
+user2.email = 'tyler.mcginnis@devmounta.in';
 
 
 
